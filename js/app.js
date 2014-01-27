6d2916ba0178a19895109b3c50cc71675a08ae7f
jikimlucas <jikimlucas@me.com>
2014-01-27 16:01:36 +0300

    updated with tagger message display.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,30 @@
+     var logger = new Logger('log');
+             Logger.prototype.clear = function() {
+            this.el.textContent = '';
+        };
 
-    Logger.prototype.clear = function() {
-        this.el.textContent = '';
-    };
+        function Logger(id) {
+            this.el = document.getElementById(id);
+        }
 
-    function Logger(id) {
-        this.el = document.getElementById(id);
-    }
+        Logger.prototype.log = function(msg, opt_class) {
+            logger.clear();
+            var fragment = document.createDocumentFragment();
+            var p = document.createElement('div');
+            p.className = opt_class || 'info';
+            p.innerHTML = msg;
+            fragment.appendChild(p);
+            $("table").fadeIn("slow");
+            this.el.appendChild(fragment);
+        };
 
-    Logger.prototype.log = function(msg, opt_class) {
-        logger.clear();
-        var fragment = document.createDocumentFragment();
-        var p = document.createElement('div');
-        p.className = opt_class || 'info';
-        p.innerHTML = msg;
-        fragment.appendChild(p);
-        $("table").fadeIn("slow");
-        this.el.appendChild(fragment);
-    };
+        Logger.prototype.clear = function() {
+            $("table").slideDown();
+            if(this.el != null){
+                this.el.textContent = '';
+            }
 
-    Logger.prototype.clear = function() {
-        $("table").slideDown();
-        this.el.textContent = '';
-    };
-
-    var logger = new Logger('log');
+        };
 
     $(document).ready(function() {
         var refreshRate = 5000;
@@ -61,14 +62,14 @@
             if(list[0].nominal_labels != null || typeof list[0].nominal_labels != 'undefined'){
                 var template = '';
                 template = template +  '<table>';
-                template = template +  '<tr><td colspan="2" class="header">' + list[0].crisis_name + '</td></tr>';
+                template = template +  '<tr><td colspan="2" class="header">Crisis : ' + list[0].crisis_name + '</td></tr>';
                 template = template +  '<tr><td colspan="2" class="tweet">' + list[0].text + '</td></tr>';
 
                 var appList =  list[0].nominal_labels== null ? [] : (list[0].nominal_labels instanceof Array ? list[0].nominal_labels : [list[0]].nominal_labels);
 
                 $.each(appList, function(index, item) {
 
-                    template = template +  '<tr><td class="msg" width="100px">Label/Confidence </td><td class="msg">' + item.label_name + '  ( Confidence : ' +  item.confidence  + ' )</td></tr>';
+                    template = template +  '<tr><td class="msg" width="100px">'+item.attribute_name+' </td><td class="msg">' + item.label_name + '  ( Confidence : ' +  roundNumber(item.confidence)  + ' )</td></tr>';
                 });
                 template =  template + '</table>';
                 logger.log(template);
@@ -80,5 +81,10 @@
         else{
             FailedRenderList();
         }
+    }
+
+    function roundNumber(num){
+        return num.toFixed(1);
+        //return (num.toString().indexOf(".") !== -1) ? num.toFixed(1) : num;
     }
 
