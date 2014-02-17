36d76bd72d494bad5eabff2cbe3a9a8c1787d26d
jikimlucas <jikimlucas@me.com>
2014-02-17 11:40:25 +0300

    created new index and keep old one

--- js/appOriginal.js
+++ js/appOriginal.js
@@ -61,7 +61,7 @@
         if(typeof list !='undefined' || list!= null ){
             if(list[0].nominal_labels != null || typeof list[0].nominal_labels != 'undefined'){
                 var template = '';
-                template = template +  '<table style="padding: 5px 5px 5px 5px;">';
+                template = template +  '<table>';
                 template = template +  '<tr><td colspan="2" class="header"><a href="http://aidr-dev.qcri.org/AIDROutput/aidrTaggerLatest.html?crisisCode='+list[0].crisis_code+'">Crisis : ' + list[0].crisis_name + '</a></td></tr>';
                 template = template +  '<tr><td colspan="2" class="tweet">' + list[0].text + '</td></tr>';
 
